4af6d0ed4a67f3731e98d09c5a2599a0ed18f9ec
David Oliveira <dol.jpo@hotmail.com>
2020-02-20 23:29:13 -0300

    Adaptacao inicial para projeto alurapic

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>{{ titulo }}</h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="foto in fotos" :key="foto">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
     </ul>
   </div>
 </template>
@@ -24,37 +14,20 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      titulo: 'Alura pic',
+      fotos: [
+        {
+          url: 'https://images.unsplash.com/photo-1582133724133-3c8661ad6832?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+          titulo: 'foto 1'
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1582156658814-74ec690e27a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=332&q=80',
+          titulo: 'foto 1'
+        }        
+      ]
     }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
