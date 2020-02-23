ca2c85611a0e8ab1b05a5b2dd3a0a7e44dc9fd39
David Oliveira <dol.jpo@hotmail.com>
2020-02-22 22:58:47 -0300

    Criado componente de Painel

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,60 @@
-
-
 <template>
   <div class="corpo" id="app">
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto in fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-          </div>
-        </div>
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: "app",
-  data() {
-    return {
-      titulo: "Alura pic",
-      fotos: []
-    };
-  },
+  import Painel from './components/shared/painel/Painel.vue';
 
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(
-        fotosApi => (this.fotos = fotosApi),
-        err => console.log(err)
-      );
-  }
-};
+  export default {
+    components: {
+      'meu-painel': Painel
+    },
+
+    name: "app",
+    data() {
+      return {
+        titulo: "Alura pic",
+        fotos: []
+      };
+    },
+
+    created() {
+      this.$http
+        .get("http://localhost:3000/v1/fotos")
+        .then(res => res.json())
+        .then(
+          fotosApi => (this.fotos = fotosApi),
+          err => console.log(err)
+        );
+    }
+  };
 </script>
 
 <style>
-.corpo {
-  font-family: Helvetica, sans-serif;
-  widows: 96%;
-  margin: 0 auto;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    widows: 96%;
+    margin: 0 auto;
+  }
 
-.centralizado {
-  text-align: center;
-}
+  .centralizado {
+    text-align: center;
+  }
 
-.lista-fotos {
-  list-style: none;
-}
+  .lista-fotos {
+    list-style: none;
+  }
 
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
-}
-
-.imagem-responsiva {
-  width: 100%
-}
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
 </style>
