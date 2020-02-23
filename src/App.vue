7357dfce4b8d8b5377427bc9c148fbbba4291904
David Oliveira <dol.jpo@hotmail.com>
2020-02-22 23:27:47 -0300

    Adicionado filtro de busca de fotos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="corpo" id="app">
     <h1 class="centralizado">{{ titulo }}</h1>
+    <input
+      class="filtro"
+      type="search"
+      placeholder="filtre por parte do título"
+      v-on:input="filtro = $event.target.value"
+    />
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotos">
+      <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
         </meu-painel>
       </li>
     </ul>
@@ -12,49 +18,66 @@
 </template>
 
 <script>
-  import Painel from './components/shared/painel/Painel.vue';
+import Painel from "./components/shared/painel/Painel.vue";
 
-  export default {
-    components: {
-      'meu-painel': Painel
-    },
+export default {
+  components: {
+    "meu-painel": Painel
+  },
 
-    name: "app",
-    data() {
-      return {
-        titulo: "Alura pic",
-        fotos: []
-      };
-    },
+  name: "app",
+  data() {
+    return {
+      titulo: "Alura pic",
+      fotos: [],
+      filtro: ""
+    };
+  },
 
-    created() {
-      this.$http
-        .get("http://localhost:3000/v1/fotos")
-        .then(res => res.json())
-        .then(
-          fotosApi => (this.fotos = fotosApi),
-          err => console.log(err)
-        );
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
     }
-  };
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(
+        fotosApi => (this.fotos = fotosApi),
+        err => console.log(err)
+      );
+  }
+};
 </script>
 
 <style>
-  .corpo {
-    font-family: Helvetica, sans-serif;
-    widows: 96%;
-    margin: 0 auto;
-  }
+.corpo {
+  font-family: Helvetica, sans-serif;
+  widows: 96%;
+  margin: 0 auto;
+}
 
-  .centralizado {
-    text-align: center;
-  }
+.centralizado {
+  text-align: center;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
 </style>
