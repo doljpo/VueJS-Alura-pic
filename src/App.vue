07c4c45e3a8d03f27c8eb42e8e83569f14c29131
David Oliveira <dol.jpo@hotmail.com>
2020-02-22 22:09:24 -0300

    Adicionado estilo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
+
+
 <template>
-  <div id="app">
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo" id="app">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
+        <div class="painel">
+          <h2 class="painel-painel">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -11,20 +18,67 @@
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      titulo: 'Alura pic',
+      titulo: "Alura pic",
       fotos: []
-    }
+    };
   },
 
-  created () {
-    this.$http.get('http://localhost:3000/v1/fotos')
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotosApi => this.fotos = fotosApi, err => console.log(err));
+      .then(
+        fotosApi => (this.fotos = fotosApi),
+        err => console.log(err)
+      );
   }
-}
+};
 </script>
 
-<style></style>
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  widows: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+.imagem-responsiva {
+  width: 100%
+}
+</style>
